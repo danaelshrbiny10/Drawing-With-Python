4cc7fb7b6676b550c4fb3c6224a720340445eda3
danaelshrbiny10 <https://github.com/danaelshrbiny10/hue_lights.git>
2021-08-23 01:29:54 +0200

    drawing circles

---
+++ circles.py
@@ -0,0 +1,20 @@
+import turtle
+# import sys
+
+
+
+turtle.bgcolor("black")
+turtle.pensize(2)
+turtle.speed(0)
+
+
+for i in range(6):
+    for colours in ("red" , "magenta", "blue","cyan","green","yellow","white"):
+        turtle.color(colours)
+        turtle.circle(100)
+        turtle.left(10)
+
+turtle.hideturtle()        
+place_turtles()
+
+# sys.exit()
